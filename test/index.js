df044de188d2976225edecc0009550e90bef80ba
chenhanjie <chenhanjie@mucfc.com>
2017-04-21 22:48:34 +0800

    chore: update test        use JSON.stringify to avoid "Cannot convert object to primitive value" error

--- test/index.js
+++ test/index.js
@@ -5,10 +5,27 @@
 
 const Fractal = require('../src/index');
 
-function Log() {
-  console.log('test:', ...arguments);
+function Log(fractal, ...args) {
+  console.log('test:', JSON.stringify(fractal), ...args);
 };
 
+Log('Object property test');
 Log(Fractal);
+Log(Fractal.a);
+Log(Fractal.a.aa);
+Log(Fractal['b']);
+Log(Fractal['b']['bb']);
+
+Log('Function call test');
+Log(Fractal());
+Log(Fractal()());
+Log(Fractal.c());
+Log(Fractal.c.cc());
+
+Log('mixin call test');
+Log(Fractal.d());
+Log(Fractal.d().d);
+Log(Fractal().d);
+Log(Fractal().d());
 
 Log('='.repeat(30));
