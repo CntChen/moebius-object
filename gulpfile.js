f5356a045312942fc691ba9f73435e1cf6b40e3b
chenhanjie <chenhanjie@mucfc.com>
2017-04-21 17:23:09 +0800

    chore: add gulp watch for auto run test

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const exec = require('child_process').exec;
+
+const srcPath = './src/*';
+const testPath = './test/index.js';
+gulp.task('test', function() {
+  const shellCommands = [
+    `node ${testPath}`,
+    ].join(' && ');
+  exec(shellCommands, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error:\${error}`);
+      return;
+    }
+    
+    if(stderr){
+      console.log(`stderr:\n${stderr}`);
+      return;
+    }
+
+    console.log(`stdout:\n${stdout}`);
+  });
+});
+
+gulp.task('watch', function() {
+  console.log('change');
+  gulp.watch(srcPath, ['test']);
+  gulp.watch(testPath, ['test']);
+});
